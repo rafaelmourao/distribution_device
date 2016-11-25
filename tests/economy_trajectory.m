805c550c7dad0c91dd79a354366653e5fdd377e8
Rafael Mourão <rafael@mourao.org>
2016-11-25 14:04:29 +0100

    Added trajectory file
    I'm still not sure about how to handle 'phi' in this case.

---
+++ tests/economy_trajectory.m
@@ -0,0 +1,63 @@
+function econ = economy_trajectory(Economy,id_br,id_bf,state,...
+        num_periods, seed)
+    
+    if (exist('seed','var') && (seed > 0))
+        rng(seed)
+    end
+    
+    n_tot_bonds_size = size(Economy.Vo);
+    states = markov_chain(Economy.prob,state,num_periods);
+    idx_s = 0 * states;
+    idx_s(1) = sub2ind(n_tot_bonds_size,state,id_br,id_bf);
+    
+    for i = 1:num_periods-1
+        if Economy.z(idx_s(i))
+            id_br_s = find(Economy.grid.b_r == Economy.br(idx_s(i)),1);
+            id_bf_s = find(Economy.grid.b_f == Economy.bf(idx_s(i)),1);
+        else
+            id_br_s = 1;
+            id_bf_s = 1;
+        end
+        idx_s(i+1) = sub2ind(n_tot_bonds_size,states(i+1),id_br_s,id_bf_s);
+    end
+    
+    econ.idx = idx_s;
+    econ.states = states;
+    
+    % finding the periods with default
+    econ.z = Economy.z(idx_s);
+    def_cases = find(~econ.z);
+    def_cases_states = states(def_cases);
+    
+    % case without default
+    fields = {'br','bf','bg','r','w','cr','cf','kr','kf','g'};
+    for i = 1:length(fields)
+        field_name = fields{i};
+        econ.(field_name) = Economy.(field_name)(idx_s);
+    end
+    
+    %cases with default
+    fields = {'r','w','cr','cf','g'};
+    for i = 1:length(fields)
+        econ.(field_name)(def_cases) = ...
+            Economy.default.(field_name)(def_cases_states);
+    end
+    econ.br(def_cases) = 0;
+    econ.bg(def_cases) = 0;
+    econ.bf(def_cases) = 0;
+end
+
+function chain = markov_chain(transition, first_state, length_chain)
+    chain = [first_state, zeros(1,length_chain-1)];
+    r = rand(1,length_chain);
+    cum_probs = cumsum(transition,2);
+    if any(cum_probs(:,end) ~= 1)
+        error('Invalid transition matrix, lines must sum to 1!')
+    end
+    for i = 2:length(chain)
+        chain(i) = find(cum_probs(chain(i-1),:) > r(i), 1);
+    end
+end
+
+
+
